2efc7309e787523ec0ee1e87cf517d4be3d66aa7
Thales Burakowski <thales.burakowski@fcamara.com.br>
2019-03-01 10:25:41 -0300

    first commit

---
+++ InventoryService/Services/IInventoryServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InventoryService.Models;
+
+namespace InventoryService.Services
+{
+    public interface IInventoryServices
+    {
+        InventoryItems AddInventoryItems(InventoryItems items);
+
+        Dictionary<string, InventoryItems> GetInventoryItems();
+    }
+}
---
+++ InventoryService/Services/InventoryServices.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InventoryService.Models;
+
+namespace InventoryService.Services
+{
+    public class InventoryServices : IInventoryServices
+    {
+        private readonly Dictionary<string, InventoryItems> _inventoryItems;
+
+        public InventoryServices()
+        {
+            _inventoryItems = new Dictionary<string, InventoryItems>();
+        }
+
+        public Dictionary<string, InventoryItems> GetInventoryItems()
+        {
+            return _inventoryItems;
+        }
+
+        InventoryItems IInventoryServices.AddInventoryItems(InventoryItems items)
+        {
+            _inventoryItems.Add(items.ItemName, items);
+
+            return items;
+        }
+    }
+}
